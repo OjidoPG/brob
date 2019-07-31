d6c9b94c10e2b546c511abf0d4edde6ae63755ae
Pascal <giraud.pascal@outlook.com>
2019-08-01 00:21:15 +0200

    scaffold controllers

---
+++ app/controllers/AdministrateursController.php
@@ -0,0 +1,14 @@
+<?php
+
+use Phalcon\Mvc\Controller;
+
+class AdministrateursController extends Controller
+{
+
+    public function indexAction()
+    {
+
+    }
+
+}
+
---
+++ app/controllers/EmplacementsController.php
@@ -0,0 +1,14 @@
+<?php
+
+use Phalcon\Mvc\Controller;
+
+class EmplacementsController extends Controller
+{
+
+    public function indexAction()
+    {
+
+    }
+
+}
+
