0d4f8ab19eda6abf135f8bd0bb28c9912ab8f66e
Pascal <giraud.pascal@outlook.com>
2019-08-08 00:57:49 +0200

    emplacement non occupe envoye au front

--- app/controllers/EmplacementsController.php
+++ app/controllers/EmplacementsController.php
@@ -7,7 +7,7 @@ class EmplacementsController extends Controller
 
     public function getEmplacementsAction()
     {
-        $listeEmplacements = Emplacements::find();
+        $listeEmplacements = Emplacements::find("occupe = 0");
         return json_encode($listeEmplacements);
     }
 }
