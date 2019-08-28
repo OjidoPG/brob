885397b41bfa0eee0f6426cf5a35f4d5eb19e858
Pascal <giraud.pascal@outlook.com>
2019-08-28 19:59:30 +0200

    ajout acl marche pas

--- app/controllers/ControllerBase.php
+++ app/controllers/ControllerBase.php
@@ -1,5 +1,6 @@
 <?php
 
+use Phalcon\Dispatcher;
 use Phalcon\Http\Response\StatusCode;
 use Phalcon\Mvc\Controller;
 
@@ -11,6 +12,29 @@ class ControllerBase extends Controller
     {
         $this->response->setStatusCode($code, StatusCode::message($code));
         return json_encode($response);
+    }
+
+    public function beforeExecuteRoute(Dispatcher $dispatcher)
+    {
+        $controllerName = $dispatcher->getControllerName();
+        $actionName = $dispatcher->getActionName();
+        $identity = $this->auth->getIdentity();
+
+        if (!$this->acl->isAllowed($identity['profile'], $controllerName, $actionName)) {
+            $this->flash->notice('You don\'t have access to this module: ' . $controllerName . ':' . $actionName);
+            if ($this->acl->isAllowed($identity['profile'], $controllerName, 'index')) {
+                $dispatcher->forward([
+                    'controller' => $controllerName,
+                    'action' => 'index'
+                ]);
+            } else {
+                $dispatcher->forward([
+                    'controller' => 'user_control',
+                    'action' => 'index'
+                ]);
+            }
+            return false;
+        }
     }
 
     public function afterExecuteRoute()
---
+++ app/models/Acl.php
@@ -0,0 +1,34 @@
+<?php
+
+use Phalcon\Acl\Adapter\Memory as AclList;
+use Phalcon\Acl\Role;
+use Phalcon\Acl\Resource;
+
+/**
+ * Gère les accès aux différentes parties de l'application
+ * Class Acl
+ */
+class Acl extends ControllerBase
+{
+    public function getAcl()
+    {
+        $acl = new AclList();
+
+        $acl->setDefaultAction(Acl::DENY);
+
+        $roleAdmininistrateurs = new Role('RoleAdministrateurs');
+        $roleClients = new Role('RoleClients');
+
+        $acl->addRole($roleAdmininistrateurs);
+        $acl->addRole($roleClients);
+
+        $adminResource = new Resource('Administrateurs');
+        $acl->addResource(
+            $adminResource,'postAdmins'
+        );
+
+        $acl->allow('RoleAdministrateurs', 'Administrateurs','postAdmins');
+        $acl->deny('RoleClients', 'Administrateurs','postAdmins');
+
+    }
+}
